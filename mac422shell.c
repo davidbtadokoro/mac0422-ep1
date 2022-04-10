42794f330fedb2ce322c3f41785b16b2c8c90897
David de Barros Tadokoro <davidbtadokoro@usp.br>
2022-04-10 19:16:55 -0300

    Adiciona close(STD_IN) de 'rode' e refatora um pouco o código

--- mac422shell.c
+++ mac422shell.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 
 #define PAL_MAX 100
+#define STD_IN 0
 
 int main(int argc, char const *argv[]) {
   char comando[PAL_MAX];
@@ -14,36 +14,37 @@ int main(int argc, char const *argv[]) {
   // Inicio da shell:
   while (1) {
     printf("mac422shell> ");
+
+    // Captura de comandos da shell:
     scanf("%s", comando);
     if (strcmp(comando, "sair") == 0) return 0; // termina shell
     scanf("%s", arquivo);
 
+    // Argumentos p/ execve():
+    char* const args[] = {arquivo, NULL};
+    char* const env[] = {NULL};
+
     // Bloco de protecao de arquivos:
     if (strcmp(comando, "protegepracaramba") == 0)
       chmod(arquivo, 0000);
-    if (strcmp(comando, "liberageral") == 0)
+    else if (strcmp(comando, "liberageral") == 0)
       chmod(arquivo, 0777);
-
-    // Bloco de execução de programas:
-    char* const args[] = {arquivo, NULL}; // prepara args de execve
-    char* const env_args[] = {NULL};      // prepara env de execve
-    if (strcmp(comando, "rodeveja") == 0) {
+    // Bloco de execucao de programas:
+    else if (strcmp(comando, "rodeveja") == 0) {
       int codigo;
       pid_t pid = fork();
-      if (pid == 0) {
-        execve(args[0], args, env_args);
-        exit(0);
-      }
-      waitpid(pid, &codigo, 0);
-      codigo = WEXITSTATUS(codigo);
-      printf("programa '%s' retornou com o codigo %d.\n", arquivo, codigo);
+      if (pid == 0) // filho cumpre predicado
+        execve(args[0], args, env);
+      waitpid(pid, &codigo, 0); // espera processo criado acima terminar
+      printf("programa '%s' retornou com o codigo %d.\n", arquivo, WEXITSTATUS(codigo));
     }
-    if (strcmp(comando, "rode") == 0) {
-      if (fork() == 0) {
-        execve(args[0], args, env_args);
-        exit(0);
+    else if (strcmp(comando, "rode") == 0) {
+      if (fork() == 0)  { // filho cumpre predicado
+        close(STD_IN);  // fecha STD_IN p/ shell monopolizar teclado
+        execve(args[0], args, env);
       }
     }
+    else printf("Comando invalido!\n");
   }
 
   return 0;
